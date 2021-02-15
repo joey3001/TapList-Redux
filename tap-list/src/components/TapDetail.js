aaa5cdafac1695c7aeeeb5647702b9a60faa5f54
Joey3001 <josephnero111@gmail.com>
2021-02-14 16:48:09 -0800

    Add README.md

--- tap-list/src/components/TapDetail.js
+++ tap-list/src/components/TapDetail.js
@@ -11,13 +11,11 @@ function TapDetail(props) {
     <React.Fragment>
       {message}
       <h1>tap Detail</h1>
-      <h3>{tap.name}</h3>
-      <h3>{tap.brand}</h3>
-      <h3>{tap.pintsLeft}</h3>
-      <h3>{tap.pricePerPint}</h3>
-      <button onClick={() => onClickingSell(tap.id)}>Sell 1 pint</button>
-      {/* <button onClick={onClickingEdit}>edit tap</button>
-      <button onClick={() => onClickingDelete(tap.id)}>delete tap</button> */}
+      <h3>Name: {tap.name}</h3>
+      <h3>Brand: {tap.brand}</h3>
+      <h3>Pints Left: {tap.pintsLeft}</h3>
+      <h3>Price per Pint: {tap.pricePerPint}</h3>
+      <button className='btn btn-primary' onClick={() => onClickingSell(tap.id)}>Sell 1 pint</button>
       <hr />
     </React.Fragment>
   );
