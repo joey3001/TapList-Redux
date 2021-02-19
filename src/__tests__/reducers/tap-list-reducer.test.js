08f2562853a4c35d25f05421d38ebddce653cbf3
Joey3001 <josephnero111@gmail.com>
2021-02-19 14:19:33 -0800

    Create 2nd test. Get good fail.

--- src/__tests__/reducers/tap-list-reducer.test.js
+++ src/__tests__/reducers/tap-list-reducer.test.js
@@ -1,7 +1,39 @@
 import tapListReducer from '../../reducers/tap-list-reducer'; 
 
 describe('tapListReducer', () => {
+  let action; 
+  const tapData ={
+    name: 'Tap1',
+    brand: 'Brand1',
+    flavor: 'Flavor1',
+    pintsLeft: 124, //Starting value for pints left in a tap
+    pricePerPint: '12',
+    id: 1
+  }
   test('Should return default state if there is no action type passed into the reducer', () => {
       expect(tapListReducer({}, {type: null})).toEqual({});
   }); 
+  test('Should successfully add new tap data to the tapList', () =>{
+      const { name, brand, flavor, pintsLeft, pricePerPint, id } = tapData;
+      action ={
+        type: 'ADD_TICKET',
+        name: name,
+        brand: brand,
+        flavor: flavor, 
+        pintsLeft: pintsLeft, 
+        pricePerPint: pricePerPint,
+        id: id
+      }; 
+
+      expect(tapListReducer({}, action)).toEqual({
+        [id] : {
+          name: name, 
+          brand: brand, 
+          flavor: flavor, 
+          pintsLeft: pintsLeft, 
+          pricePerPint: pricePerPint,
+          id: id
+        }
+      })
+  })
 });
