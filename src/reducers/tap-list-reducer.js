5a19bfb4c5455480b35ef308c226bb3f9d84cd2c
Joey3001 <josephnero111@gmail.com>
2021-02-19 14:26:49 -0800

    Add code to reducer. Pass 2nd test.

--- src/reducers/tap-list-reducer.js
+++ src/reducers/tap-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state ={}, action) => {
-  return state; 
+  const { name, brand, flavor, pintsLeft, pricePerPint, id } = action; 
+  switch (action.type) {
+    case 'ADD_TICKET':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          flavor: flavor, 
+          pintsLeft: pintsLeft, 
+          pricePerPint: pricePerPint,
+          id: id
+        }
+      })
+    default: 
+      return state;
+  }
 }; 
